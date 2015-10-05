2475a98816de1a13fccfd16a2d285b641b66b3d5
Tischenko Sergiy <sytischenko@gmail.com>
2015-10-06 01:20:06 +0300

    Create conversion.py
    conversion function for previous device control

---
+++ conversion.py
@@ -0,0 +1,19 @@
+import os
+
+def conversion(command):
+    #Get current coord (x , y) from command adb shell input tap x y
+    to_int = lambda lst: [int(i) for i in lst]
+    x1, y1 = to_int(command.split()[-2:])
+    x_bm, y_bm = 1280, 800 # base width, height max size
+    # get max coordinates x,y for tested device " Physical size: 480x800"
+    answer = os.popen('adb shell wm size').readline()
+    x_cm, y_cm = to_int((answer.split()[-1]).split('x'))
+    ky1 = x_cm/x_bm
+    kx1 = y_cm/y_bm
+    x2 = x_cm-y1*kx1
+    y2 = kx1*x1
+    print 'Coord: old: (%s,%s)/ new: (%s,%s)'%(x1,y1,x2,y2)
+    return os.popen('adb shell input tap %i %i'%(x2,y2))
+    
+os.system = conversion
+os.system('adb shell input tap 165 250')
